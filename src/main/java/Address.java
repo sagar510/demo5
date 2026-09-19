43836cd2fae8f4bc32021ccfa7cc7c8d893fa2b8
sagar510 <sagarpal786t@gmail.com>
2026-09-19 18:09:36 +0530

    Initial commit

---
+++ src/main/java/Address.java
@@ -0,0 +1,11 @@
+public class Address {
+    private String houseNo;
+    private String street;
+    private String pinCode;
+
+    public Address(String houseNo, String street, String pinCode) {
+        this.houseNo = houseNo;
+        this.street = street;
+        this.pinCode = pinCode;
+    }
+}
---
+++ src/main/java/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private String name;
+    private float salary;
+    private Address address;
+
+    public Employee(String name, float salary, Address address) {
+        this.name = name;
+        this.salary = salary;
+        this.address = address;
+    }
+}
